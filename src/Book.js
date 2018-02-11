bedc9213a1b12c0adb6f2741d9e35ec2831908bf
Balazs Szele <balazs.szele@prezi.com>
2018-02-10 19:13:15 -0800

    Fix missing thumbnails and authors

--- src/Book.js
+++ src/Book.js
@@ -4,15 +4,18 @@ class Book extends Component {
     onSelectChange = (e) => {
         this.props.changeShelf(this.props.book, e.target.value)
     };
+    getThumbnail = (book) => {
+        return (book.imageLinks && book.imageLinks.smallThumbnail) || 'http://via.placeholder.com/128x193?text=No%20Cover'
+    };
     render () {
         return (
             <li>
                 <div className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={{width: 128, height: 188, backgroundImage: `url("${this.props.book.imageLinks.smallThumbnail}")`}} />
+                        <div className="book-cover" style={{width: 128, height: 188, backgroundImage: `url("${this.getThumbnail(this.props.book)}")`}} />
                         <div className="book-shelf-changer">
                             <select onChange={this.onSelectChange} defaultValue={this.props.book.shelf || 'none'}>
-                                <option value="none" disabled>Move to...</option>
+                                <option value="disabled" disabled>Move to...</option>
                                 <option value="currentlyReading">Currently Reading</option>
                                 <option value="wantToRead">Want to Read</option>
                                 <option value="read">Read</option>
@@ -21,7 +24,7 @@ class Book extends Component {
                         </div>
                     </div>
                     <div className="book-title">{this.props.book.title}</div>
-                    <div className="book-authors">{this.props.book.author}</div>
+                    <div className="book-authors">{this.props.book.authors.join(" , ")}</div>
                 </div>
             </li>
         )
