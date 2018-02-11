66f1f1f8b44a43bf18098af2d43879271baeed3c
Balazs Szele <balazs.szele@prezi.com>
2018-02-10 19:36:58 -0800

    Fix author for no authors

--- src/Book.js
+++ src/Book.js
@@ -4,15 +4,21 @@ class Book extends Component {
     onSelectChange = (e) => {
         this.props.changeShelf(this.props.book, e.target.value)
     };
-    getThumbnail = (book) => {
-        return (book.imageLinks && book.imageLinks.smallThumbnail) || 'http://via.placeholder.com/128x193?text=No%20Cover'
+
+    getThumbnail = () => {
+        return (this.props.book.imageLinks && this.props.book.imageLinks.smallThumbnail) || 'http://via.placeholder.com/128x193?text=No%20Cover'
     };
+
+    getAuthors = () => {
+        return (this.props.book.authors && this.props.book.authors.join(', ')) || ''
+    };
+
     render () {
         return (
             <li>
                 <div className="book">
                     <div className="book-top">
-                        <div className="book-cover" style={{width: 128, height: 188, backgroundImage: `url("${this.getThumbnail(this.props.book)}")`}} />
+                        <div className="book-cover" style={{width: 128, height: 188, backgroundImage: `url("${this.getThumbnail()}")`}} />
                         <div className="book-shelf-changer">
                             <select onChange={this.onSelectChange} defaultValue={this.props.book.shelf || 'none'}>
                                 <option value="disabled" disabled>Move to...</option>
@@ -24,7 +30,7 @@ class Book extends Component {
                         </div>
                     </div>
                     <div className="book-title">{this.props.book.title}</div>
-                    <div className="book-authors">{this.props.book.authors.join(" , ")}</div>
+                    <div className="book-authors">{this.getAuthors()}</div>
                 </div>
             </li>
         )
